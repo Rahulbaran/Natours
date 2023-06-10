36ff00760d425a0f6031ceb88757e5602123bbf3
Rahulkumar <rahulkumar109422@gmail.com>
2023-06-10 20:59:52 +0530

    feat: create features for advance filtering and sorting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,37 @@ const Tour = require("../models/tourModel");
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    // 1A) Filtering
+    const queryObj = { ...req.query };
+    const excludeFields = ["page", "sort", "limit", "fields"];
+    excludeFields.forEach(el => delete queryObj[el]);
+
+    // 1B) Advance Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) SORTING
+    if (req.query.sort) {
+      query = query.sort(req.query.sort.replace(",", " "));
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    // 3) Limiting FIelds
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res
       .status(200)
       .json({ status: "success", total: tours.length, data: { tours } });
@@ -17,7 +47,6 @@ exports.getAllTours = async (req, res) => {
 exports.getTour = async (req, res) => {
   try {
     const tour = await Tour.findById(req.params.id);
-    // const tour = await Tour.findOne({ _id: req.params.id });
 
     res.status(200).json({
       status: "success",
