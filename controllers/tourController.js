c42be2e108dc32b1d51937cc9cf16225025f7a4d
Rahulkumar <rahulkumar109422@gmail.com>
2023-06-20 15:11:22 +0530

    feat: add alias route for getting top 5 cheapest tours
    - created new route "top-5-cheap" which takes two middlewares
    - created a middleware for modifying request object

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,12 @@
 const Tour = require("../models/tourModel");
 
+exports.aliasTopTours = async (req, res, next) => {
+  req.query.limit = "5";
+  req.query.sort = "-ratingsAverage,price";
+  req.query.fields = "name,price,difficulty,ratingsAverage,summary";
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     // BUILD QUERY
