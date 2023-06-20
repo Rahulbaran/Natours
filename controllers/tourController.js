866c86757736242340965d07f27d5d292988b5a1
Rahulkumar <rahulkumar109422@gmail.com>
2023-06-20 15:10:11 +0530

    feat: add the feature of pagination and limiting results

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -21,12 +21,24 @@ exports.getAllTours = async (req, res) => {
       query = query.sort("-createdAt");
     }
 
-    // 3) Limiting FIelds
+    // 3) Limiting Fields
     if (req.query.fields) {
       const fields = req.query.fields.split(",").join(" ");
       query = query.select(fields);
     } else {
       query = query.select("-__v");
+    }
+
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 10;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit * 1);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw RangeError("This page does not exist");
     }
 
     // EXECUTE QUERY
